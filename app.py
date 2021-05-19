d134b7e70db264defe0e6af3fdc22748d3e6eac7
bannyclass <bannyclass@gmail.com>
2021-05-19 13:41:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def processRequest(req):
 	    
     	
             
-        fulfillmentText= "This message is a {} ! Thank you for using SpamBot Would you like to verify another message?".format(msg_status)
+        fulfillmentText= "This message is a {} ! Thank you for using SpamBot Would you like to verify another message?".format(prediction)
         #log.write_log(sessionID, "Bot Says: "+fulfillmentText)
         return {
             "fulfillmentText": fulfillmentText
