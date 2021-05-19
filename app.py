887633f8f76ae42b50a9a543a803136818f9250a
bannyclass <bannyclass@gmail.com>
2021-05-19 08:42:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from flask import Flask, request, make_response
 import json
 import pickle
 from flask_cors import cross_origin
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.text import tokenizer_from_json
-from tensorflow.keras.preprocessing import sequence
-import json
+
+
 
 app = Flask(__name__)
 #model = pickle.load(open('rf.pkl', 'rb'))
@@ -44,42 +42,48 @@ def processRequest(req):
     #log.write_log(sessionID, "User Says: "+user_says)
     parameters = result.get("parameters")
     message=parameters.get("msg")
-	
 
-    tokenizer = None
-    with open('./spam_tokenizer.json') as f:
-        data = json.load(f)
-        tokenizer = tokenizer_from_json(data)
-    tokenizer
-
-    sen = tokenizer.texts_to_sequences(message)
-    
-
-    text_matrix = sequence.pad_sequences(sen,maxlen=max_len)
-    
-
-
-
+	 
     intent = result.get("intent").get('displayName')
-
+    
     if (intent=='yes'):
+        
+        tokenizer = None
+        with open('./spam_tokenizer.json') as f:
+            data = json.load(f)
+            tokenizer = tokenizer_from_json(data)
+        tokenizer
+
+        sen = tokenizer.texts_to_sequences(text)
+        sen
+
+        text_matrix = sequence.pad_sequences(sen,maxlen=max_len)
+        text_matrix
+
+       
+
+        
+        
+        
         prediction = loaded_model.predict(text_matrix)
-
-
-
-        if(prediction<=0.5):
+    
+        output = round(prediction[0], 2)
+    
+    	
+        if(output<=0.5):
             msg_status = 'Ham'
-
-        if(prediction>=0.5):
-            msg_status = 'Spam'
-
+    
+        if(output>=0.5):
+             msg_status = 'Spam'
+        
             
         fulfillmentText= "The Message appears to be..  {} !".format(msg_status)
         #log.write_log(sessionID, "Bot Says: "+fulfillmentText)
         return {
             "fulfillmentText": fulfillmentText
         }
-
+    #else:
+    #    log.write_log(sessionID, "Bot Says: " + result.fulfillmentText)
 
 if __name__ == '__main__':
     app.run()
@@ -87,4 +91,3 @@ if __name__ == '__main__':
 #    port = int(os.getenv('PORT', 5000))
 #    print("Starting app on port %d" % port)
 #    app.run(debug=False, port=port, host='0.0.0.0')
-
